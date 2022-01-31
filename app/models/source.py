e5f67e5150662960bd173ad56a2c730cdd5a40f8
Esther-Anyona <jkemuntoe@gmail.com>
2022-01-31 17:32:18 +0300

    Add navigation template, add article class, test and api base url for articles

--- app/models/source.py
+++ app/models/source.py
@@ -14,4 +14,13 @@ class NewsArticle:
     '''
     Class to define News articles
     '''
-    pass
+    def __init__(self, id, name, author, title, description, url, urlToImage, publishedAt, content):
+        self.id = id
+        self.name = name
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
