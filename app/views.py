0f897ea83e6d91d40ad57ef60d08eb029afe0e0d
Esther-Anyona <jkemuntoe@gmail.com>
2022-01-31 06:52:13 +0300

    Import the get news function from request module and call source as argument in views

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 # Views
 @app.route('/')
@@ -7,4 +8,6 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    source = get_news()
+    title = 'Welcome to The Bulletin'
+    return render_template('index.html', title = title, source = source)
