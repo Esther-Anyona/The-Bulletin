4d9b528c67b1ddf4936e1aaf9d803c0903a12ae9
Esther-Anyona <jkemuntoe@gmail.com>
2022-02-01 14:19:31 +0300

    Resolve errors

--- manage.py
+++ manage.py
@@ -1,13 +1,9 @@
 from app import create_app
-from flask_script import Manager,Server
 
 # Creating app instance
 app = create_app('development')
 
-manager = Manager(app)
-manager.add_command('server',Server)
 
-@manager.command
 def test():
     """
     Running unit tests
@@ -17,4 +13,4 @@ def test():
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
